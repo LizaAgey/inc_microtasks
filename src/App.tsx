7bfd5029f55f92b8379a7061cb8b217131774b3f
Liza Agey <agey.eliz@gmail.com>
2022-12-11 20:59:14 +0700

    feat: refactoring with associative arrays

--- src/App.tsx
+++ src/App.tsx
@@ -1,60 +1,92 @@
 import React, {useState} from 'react';
-import logo from './logo.svg';
 import './App.css';
-import FullInput from "./components/FullInput";
-import Input from "./components/Input";
-import Button from "./components/Button";
+import {Todolist} from './Todolist';
+import {v1} from 'uuid';
 
-export type MessageType = {
-    message: string
+export type FilterValuesType = 'all' | 'active' | 'completed';
+
+export type TodoListsType = {
+    id: string,
+    title: string,
+    filter: FilterValuesType
 }
-
 
 function App() {
 
-    let [messages, setMessages] = useState([
-        {message: "one"},
-        {message: "two"},
-        {message: "three"}
+    let todolistID1 = v1();
+    let todolistID2 = v1();
+
+    let [todolists, setTodolists] = useState<Array<TodoListsType>>([
+        {id: todolistID1, title: 'What to learn', filter: 'all'},
+        {id: todolistID2, title: 'What to buy', filter: 'all'},
     ])
 
-    //for a variant with separate components Input and Button
-    let [title, setTitle] = useState("")
+    let [tasks, setTasks] = useState({
+        [todolistID1]: [
+            {id: v1(), title: 'HTML&CSS', isDone: true},
+            {id: v1(), title: 'JS', isDone: true},
+            {id: v1(), title: 'ReactJS', isDone: false},
+            {id: v1(), title: 'Rest API', isDone: false},
+            {id: v1(), title: 'GraphQL', isDone: false},
+        ],
+        [todolistID2]: [
+            {id: v1(), title: 'HTML&CSS2', isDone: true},
+            {id: v1(), title: 'JS2', isDone: true},
+            {id: v1(), title: 'ReactJS2', isDone: false},
+            {id: v1(), title: 'Rest API2', isDone: false},
+            {id: v1(), title: 'GraphQL2', isDone: false},
+        ]
+    });
 
-    const addMessage = (title: string) => {
-        let newMessage = {message: title}
-        setMessages([newMessage, ...messages])
-    };
 
-    //for a variant with separate components Input and Button
-    const callBackOnClickButtonHandler = () => {
-        addMessage(title)
-        setTitle("")
+    function removeTask(todoListID: string, taskID: string) {
+        let filteredTasks = tasks[todoListID].filter(task => task.id != taskID);
+        setTasks({...tasks, [todoListID]: filteredTasks});
     }
+
+    function addTask(todoListID: string, title: string) {
+        let task = {id: v1(), title: title, isDone: false};
+        let newTasks = [task, ...tasks[todoListID]];
+        setTasks({...tasks, [todoListID]: newTasks});
+    }
+
+    function changeTaskStatus(todoListID: string, taskId: string, isDone: boolean) {
+        setTasks({...tasks,
+            [todoListID]: tasks[todoListID].map(task => task.id === taskId ? {...task, isDone} : task)});
+    }
+
+
+    function changeFilter(todoListID: string, filterValue: FilterValuesType) {
+        setTodolists(todolists.map(list => list.id === todoListID ? {...list, filter: filterValue} : list))
+    }
+
+
+
 
     return (
         <div className="App">
-            {/*<FullInput*/}
-            {/*    addMessage={addMessage}*/}
-            {/*/>*/}
 
-            {/*for a variant with separate components Input and Button*/}
-            <Input
-                setTitle={setTitle}
-                title={title}
-            />
-            <Button
-                name={"Send"}
-                callBack={callBackOnClickButtonHandler}
-            />
+            {todolists.map(list => {
+                let filteredTasks = tasks[list.id]
+                if(list.filter === "completed") {
+                    filteredTasks = tasks[list.id].filter(task => task.isDone == true)
+                }
+                if(list.filter === "active") {
+                    filteredTasks = tasks[list.id].filter(task => task.isDone == false)
+                }
+                return <Todolist
+                    key={list.id}
+                    todolistID={list.id}
+                    title={list.title}
+                    tasks={filteredTasks}
+                    removeTask={removeTask}
+                    changeFilter={changeFilter}
+                    addTask={addTask}
+                    changeTaskStatus={changeTaskStatus}
+                    filter={list.filter}
+                />}
+            )}
 
-
-
-            {messages.map((element, index) => {
-                return (
-                    <div key={index}>{element.message}</div>
-                )
-            })}
         </div>
     );
 }
